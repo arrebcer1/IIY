e06ca924cc880ae0ea89be1bbb46296d9b395219
Ankur Nigam <ankur.nigam198@gmail.com>
2019-02-08 22:17:51 +0530

    contact vue

---
+++ script/Contacts.js
@@ -0,0 +1,21 @@
+var x;
+function getContactsData() {
+    fetch("Data/Contact.json")
+        .then(data => data.json())
+        .then(data => {
+            console.log(data);
+            x=data;
+            createContactList(data);
+        })
+}
+
+var listElements=[]
+
+function createContactList(data){
+    var appContacts = new Vue({
+        el :"#contactsList",
+        data: {
+            contacts: data
+        }
+    });
+}
--- script/basic.js
+++ script/basic.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var menu = document.getElementById('menu')
-    menu.addEventListener('click',function(){
+    menu.addEventListener('click', function () {
         document.getElementById('menu-drawer').classList.toggle("hide-mobile")
     })
-  });
+});
